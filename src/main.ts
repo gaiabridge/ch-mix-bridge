d4b2053cd0244334de06c98f8b4ae81c814c92fb
dilrong <dilrong@dilrong.com>
2021-10-27 22:05:05 +0900

    style: 스타일 업데이트
    - sns 버튼 추가
    - 버튼 hover 효과
    - 기타 스타일 수정

--- src/main.ts
+++ src/main.ts
@@ -2,23 +2,59 @@ import { BodyNode, el } from "@hanul/skynode";
 import Swaper from "./component/Swaper";
 
 (async () => {
-    BodyNode.append(
-        el("h1",
-            el(".logo", "Chain Horizon", { href: "/" }),
-            el(".mix",
-                el("a", "Mix", {
-                    href: "https://dogesound.club/mix",
-                    target: "_blank",
-                }),
-                "Bridge"
-            ),
-            new Swaper()
+  BodyNode.append(
+    el(
+      "h1",
+      el(".logo", "Chain Horizon", { href: "/" }),
+      el(
+        ".mix",
+        el("a", "Mix", {
+          href: "https://dogesound.club/mix",
+          target: "_blank",
+        }),
+        "Bridge"
+      ),
+      new Swaper()
+    ),
+    el(
+      "footer",
+      el("a", "© Chain Horizon", {
+        href: "https://chainhorizon.org",
+        target: "_blank",
+      }),
+      el(
+        ".social",
+        el(
+          "a.item",
+          el("img", "github", {
+            src: "/images/github.png",
+            height: "32",
+          }),
+          {
+            href: "https://github.com/chainhorizon",
+            target: "_blank",
+          }
         ),
-        el("footer",
-            el("a", "© Chain Horizon", {
-                href: "https://chainhorizon.org",
-                target: "_blank",
-            })
+        el(
+          "a.item",
+          el("img", "medium", { src: "/images/medium.png", height: "32" }),
+          {
+            href: "https://medium.com/chainhorizon",
+            target: "_blank",
+          }
         ),
-    );
+        el(
+          "a.item",
+          el("img", "twitter", {
+            src: "/images/twitter.png",
+            height: "32",
+          }),
+          {
+            href: "https://chainhorizon.org",
+            target: "_blank",
+          }
+        )
+      )
+    )
+  );
 })();
