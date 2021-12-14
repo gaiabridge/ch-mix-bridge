9de9bcd5bdfc7bd002cc11d1fdb95c309cbdc405
Hanul <hanul@hanul.me>
2021-12-14 16:34:25 +0900

    안내 멘트 추가

--- src/component/Form.ts
+++ src/component/Form.ts
@@ -7,154 +7,154 @@ import PolygonMixContract from "../contracts/PolygonMixContract";
 import Swaper from "./Swaper";
 
 export default class Form extends DomNode {
-  public sender: MixSenderInterface | undefined;
+    public sender: MixSenderInterface | undefined;
 
-  private chainIcon: DomNode<HTMLImageElement>;
-  private chainSelect: DomNode<HTMLSelectElement>;
-  private balanceDisplay: DomNode;
-  private inputContainer: DomNode;
-  private buttonContainer: DomNode;
+    private chainIcon: DomNode<HTMLImageElement>;
+    private chainSelect: DomNode<HTMLSelectElement>;
+    private balanceDisplay: DomNode;
+    private inputContainer: DomNode;
+    private buttonContainer: DomNode;
 
-  constructor(
-    private swaper: Swaper,
-    public chainId: number,
-    private isFrom: boolean = false
-  ) {
-    super(".form");
-    this.append(
-      el(".chain",
-        el(".icon",
-          this.chainIcon = el("img", { height: "24" }),
-        ),
-        this.chainSelect = el(
-          "select",
-          el("option", "Klaytn", {
-            value: "8217",
-          }),
-          el("option", "Ethereum", {
-            value: "1",
-          }),
-          el("option", "Polygon", {
-            value: "137",
-          }),
-          {
-            change: () => {
-              const originChainId = this.chainId;
-              this.changeChain(parseInt(this.chainSelect.domElement.value, 10));
-              this.fireEvent("changeChain", this.chainId, originChainId);
-            },
-          }
-        ) as any,
-        isFrom ? el("span.help-text", "에서") : el("span.help-text", "으로")
-      ),
-      (this.balanceDisplay = el(".balance")),
-      (this.inputContainer = el(".input-container")),
-      (this.buttonContainer = el(".button-container"))
-    );
-    this.changeChain(chainId);
-  }
-
-  public async changeChain(chainId: number) {
-    this.chainId = chainId;
-    this.chainSelect.domElement.value = String(chainId);
-
-    this.sender?.off("connect", this.connectHandler);
-    this.sender?.off("Transfer", this.transferHandler);
-    this.sender?.off("SendOverHorizon", this.sendOverHorizonHandler);
-
-    if (chainId === 8217) {
-      this.sender = MixSenderContract;
-      this.chainIcon.domElement.src = "/images/klaytn-logo.png";
-    } else if (chainId === 1) {
-      this.sender = EthereumMixContract;
-      this.chainIcon.domElement.src = "/images/ethereum-logo.png";
-    } else if (chainId === 137) {
-      this.sender = PolygonMixContract;
-      this.chainIcon.domElement.src = "/images/polygon-logo.png";
-    }
-    await this.loadBalance();
-
-    this.sender?.on("connect", this.connectHandler);
-    this.sender?.on("Transfer", this.transferHandler);
-    this.sender?.on("SendOverHorizon", this.sendOverHorizonHandler);
-  }
-
-  private async loadBalance() {
-    this.inputContainer.empty();
-    this.buttonContainer.empty();
-
-    if (this.sender !== undefined) {
-      const owner = await this.sender.loadAddress();
-      if (owner !== undefined) {
-        const balance = await this.sender.balanceOf(owner);
-        this.balanceDisplay
-          .empty()
-          .appendText(`${utils.formatUnits(balance)} MIX`);
-
-        this.buttonContainer.append(
-          el("a.add-token-to-wallet-button", "지갑에 토큰 추가하기", {
-            click: () => {
-              this.sender?.addTokenToWallet();
-            },
-          }),
-        );
-
-        if (this.isFrom === true) {
-          const input: DomNode<HTMLInputElement> = el("input", {
-            placeholder: "보낼 수량",
-          });
-          input.appendTo(this.inputContainer);
-
-          this.buttonContainer.append(
-            el("a.send-button", "보내기", {
-              click: () =>
-                this.swaper.sendOverHorizon(
-                  utils.parseEther(input.domElement.value)
+    constructor(
+        private swaper: Swaper,
+        public chainId: number,
+        private isFrom: boolean = false
+    ) {
+        super(".form");
+        this.append(
+            el(".chain",
+                el(".icon",
+                    this.chainIcon = el("img", { height: "24" }),
                 ),
-            })
-          );
-        }
-      } else {
-        this.balanceDisplay.empty().appendText("잔액 불러오기 실패");
-        this.buttonContainer.append(
-          el("a.connect-button", "지갑 연결", {
-            click: () => this.sender?.connect(),
-          })
+                this.chainSelect = el(
+                    "select",
+                    el("option", "Klaytn", {
+                        value: "8217",
+                    }),
+                    el("option", "Ethereum", {
+                        value: "1",
+                    }),
+                    el("option", "Polygon", {
+                        value: "137",
+                    }),
+                    {
+                        change: () => {
+                            const originChainId = this.chainId;
+                            this.changeChain(parseInt(this.chainSelect.domElement.value, 10));
+                            this.fireEvent("changeChain", this.chainId, originChainId);
+                        },
+                    }
+                ) as any,
+                isFrom ? el("span.help-text", "에서") : el("span.help-text", "으로")
+            ),
+            (this.balanceDisplay = el(".balance")),
+            (this.inputContainer = el(".input-container")),
+            (this.buttonContainer = el(".button-container"))
         );
-      }
+        this.changeChain(chainId);
     }
-  }
 
-  private connectHandler = async () => {
-    this.fireEvent("connect");
-    this.loadBalance();
-  };
+    public async changeChain(chainId: number) {
+        this.chainId = chainId;
+        this.chainSelect.domElement.value = String(chainId);
 
-  private transferHandler = async (from: string, to: string) => {
-    const owner = await this.sender?.loadAddress();
-    if (from === owner || to === owner) {
-      this.loadBalance();
+        this.sender?.off("connect", this.connectHandler);
+        this.sender?.off("Transfer", this.transferHandler);
+        this.sender?.off("SendOverHorizon", this.sendOverHorizonHandler);
+
+        if (chainId === 8217) {
+            this.sender = MixSenderContract;
+            this.chainIcon.domElement.src = "/images/klaytn-logo.png";
+        } else if (chainId === 1) {
+            this.sender = EthereumMixContract;
+            this.chainIcon.domElement.src = "/images/ethereum-logo.png";
+        } else if (chainId === 137) {
+            this.sender = PolygonMixContract;
+            this.chainIcon.domElement.src = "/images/polygon-logo.png";
+        }
+        await this.loadBalance();
+
+        this.sender?.on("connect", this.connectHandler);
+        this.sender?.on("Transfer", this.transferHandler);
+        this.sender?.on("SendOverHorizon", this.sendOverHorizonHandler);
     }
-  };
 
-  private sendOverHorizonHandler = async (
-    sender: string,
-    toChain: BigNumber,
-    receiver: string,
-    sendId: BigNumber,
-    amount: BigNumber
-  ) => {
-    this.swaper.receiveOverHorizon(receiver, toChain, sender, sendId, amount);
-    const owner = await this.sender?.loadAddress();
-    if (sender === owner) {
-      this.swaper.addSended(sender, receiver, sendId);
+    private async loadBalance() {
+        this.inputContainer.empty();
+        this.buttonContainer.empty();
+
+        if (this.sender !== undefined) {
+            const owner = await this.sender.loadAddress();
+            if (owner !== undefined) {
+                const balance = await this.sender.balanceOf(owner);
+                this.balanceDisplay
+                    .empty()
+                    .appendText(`${utils.formatUnits(balance)} MIX`);
+
+                this.buttonContainer.append(
+                    el("a.add-token-to-wallet-button", "지갑에 토큰 추가하기", {
+                        click: () => {
+                            this.sender?.addTokenToWallet();
+                        },
+                    }),
+                );
+
+                if (this.isFrom === true) {
+                    const input: DomNode<HTMLInputElement> = el("input", {
+                        placeholder: "보낼 수량",
+                    });
+                    input.appendTo(this.inputContainer);
+
+                    this.buttonContainer.append(
+                        el("a.send-button", "보내기", {
+                            click: () =>
+                                this.swaper.sendOverHorizon(
+                                    utils.parseEther(input.domElement.value)
+                                ),
+                        })
+                    );
+                }
+            } else {
+                this.balanceDisplay.empty().appendText("잔액 불러오기 실패");
+                this.buttonContainer.append(
+                    el("a.connect-button", "지갑 연결", {
+                        click: () => this.sender?.connect(),
+                    })
+                );
+            }
+        }
     }
-  };
 
-  public delete() {
-    this.sender?.off("connect", this.connectHandler);
-    this.sender?.off("Transfer", this.transferHandler);
-    this.sender?.off("SendOverHorizon", this.sendOverHorizonHandler);
-    super.delete();
-  }
+    private connectHandler = async () => {
+        this.fireEvent("connect");
+        this.loadBalance();
+    };
+
+    private transferHandler = async (from: string, to: string) => {
+        const owner = await this.sender?.loadAddress();
+        if (from === owner || to === owner) {
+            this.loadBalance();
+        }
+    };
+
+    private sendOverHorizonHandler = async (
+        sender: string,
+        toChain: BigNumber,
+        receiver: string,
+        sendId: BigNumber,
+        amount: BigNumber
+    ) => {
+        this.swaper.receiveOverHorizon(receiver, toChain, sender, sendId, amount);
+        const owner = await this.sender?.loadAddress();
+        if (sender === owner) {
+            this.swaper.addSended(sender, receiver, sendId);
+        }
+    };
+
+    public delete() {
+        this.sender?.off("connect", this.connectHandler);
+        this.sender?.off("Transfer", this.transferHandler);
+        this.sender?.off("SendOverHorizon", this.sendOverHorizonHandler);
+        super.delete();
+    }
 }
--- src/component/Swaper.ts
+++ src/component/Swaper.ts
@@ -6,166 +6,164 @@ import Form from "./Form";
 import Sended from "./Sended";
 
 export default class Swaper extends DomNode {
-  private fromForm: Form;
-  private toForm: Form;
+    private fromForm: Form;
+    private toForm: Form;
 
-  private sendedList: DomNode;
+    private sendedList: DomNode;
 
-  constructor() {
-    super(".swaper");
+    constructor() {
+        super(".swaper");
 
-    this.append(
-      el(
-        ".form-container",
-        (this.fromForm = new Form(this, 8217, true)),
-        el("img.arrow", { src: "/images/arrow.png", height: "50" }),
-        el("img.mobile-arrow", { src: "/images/arrow.png", height: "50" }),
-        (this.toForm = new Form(this, 1))
-      ),
-      el(
-        ".history",
-        el("h2", "전송 기록"),
-        el("hr.divider"),
-        (this.sendedList = el(".sended-list"))
-      )
-    );
-
-    this.fromForm.on(
-      "changeChain",
-      (chainId: number, originChainId: number) => {
-        if (this.toForm.chainId === chainId) {
-          this.toForm.changeChain(originChainId);
-        }
-        this.loadHistory();
-      }
-    );
-
-    this.toForm.on("changeChain", (chainId: number, originChainId: number) => {
-      if (this.fromForm.chainId === chainId) {
-        this.fromForm.changeChain(originChainId);
-      }
-      this.loadHistory();
-    });
-
-    this.loadHistory();
-    this.fromForm.on("connect", () => this.loadHistory());
-    this.toForm.on("connect", () => this.loadHistory());
-  }
-
-  private loadHistoryNonce = 0;
-
-  private async loadHistory() {
-    if (
-      this.fromForm.sender !== undefined &&
-      this.toForm.sender !== undefined
-    ) {
-      const sender = await this.fromForm.sender.loadAddress();
-      const receiver = await this.toForm.sender.loadAddress();
-      if (sender !== undefined && receiver !== undefined) {
-        const count = await this.fromForm.sender.sendCount(
-          sender,
-          this.toForm.chainId,
-          receiver
-        );
-        this.loadHistoryNonce += 1;
-        const nonce = this.loadHistoryNonce;
-        this.sendedList.empty();
-
-        SkyUtil.repeatResultAsync(count.toNumber(), async (sendId) => {
-          if (this.loadHistoryNonce === nonce) {
-            this.addSended(sender, receiver, BigNumber.from(sendId));
-          }
-        });
-      }
-    }
-  }
-
-  public addSended(sender: string, receiver: string, sendId: BigNumber) {
-    if (
-      this.fromForm.sender !== undefined &&
-      this.toForm.sender !== undefined
-    ) {
-      new Sended(
-        this.fromForm.sender,
-        this.toForm.sender,
-        this.fromForm.chainId,
-        this.toForm.chainId,
-        sender,
-        receiver,
-        sendId.toNumber(),
-        async () => {
-          if (this.fromForm.sender !== undefined) {
-            const sended = await this.fromForm.sender.sended(
-              sender,
-              this.toForm.chainId,
-              receiver,
-              sendId
-            );
-            this.receiveOverHorizon(
-              receiver,
-              this.toForm.chainId,
-              sender,
-              sendId,
-              sended
-            );
-          }
-        }
-      ).appendTo(this.sendedList, 0);
-    }
-  }
-
-  public async sendOverHorizon(amount: BigNumberish) {
-    if (
-      this.fromForm.sender !== undefined &&
-      this.toForm.sender !== undefined
-    ) {
-      const receiver = await this.toForm.sender.loadAddress();
-      if (receiver !== undefined) {
-        await this.fromForm.sender.sendOverHorizon(
-          this.toForm.chainId,
-          receiver,
-          amount
-        );
-      }
-    }
-  }
-
-  public async receiveOverHorizon(
-    _receiver: string,
-    toChain: BigNumberish,
-    sender: string,
-    sendId: BigNumber,
-    amount: BigNumberish
-  ) {
-    if (
-      this.fromForm.sender !== undefined &&
-      this.toForm.sender !== undefined &&
-      this.toForm.chainId.toString() === toChain.toString()
-    ) {
-      const receiver = await this.toForm.sender.loadAddress();
-      if (receiver === _receiver) {
-        try {
-          const result = await superagent
-            .get(
-              `https://api.chainhorizon.org/mix/signsend?receiver=${receiver}&fromChain=${
-                this.fromForm.chainId
-              }&toChain=${
-                this.toForm.chainId
-              }&sender=${sender}&sendId=${sendId}&amount=${amount.toString()}`
+        this.append(
+            el(
+                ".form-container",
+                (this.fromForm = new Form(this, 8217, true)),
+                el("img.arrow", { src: "/images/arrow.png", height: "50" }),
+                el("img.mobile-arrow", { src: "/images/arrow.png", height: "50" }),
+                (this.toForm = new Form(this, 1))
+            ),
+            el(
+                ".history",
+                el("h2", "전송 기록"),
+                el("hr.divider"),
+                (this.sendedList = el(".sended-list"))
             )
-            .send();
-          await this.toForm.sender.receiveOverHorizon(
-            this.fromForm.chainId,
-            this.toForm.chainId,
-            sender,
-            sendId,
-            amount,
-            result.text
-          );
-        } catch (error: any) {
-          alert(`Error: ${error.message}`);
-        }
-      }
+        );
+
+        this.fromForm.on(
+            "changeChain",
+            (chainId: number, originChainId: number) => {
+                if (this.toForm.chainId === chainId) {
+                    this.toForm.changeChain(originChainId);
+                }
+                this.loadHistory();
+            }
+        );
+
+        this.toForm.on("changeChain", (chainId: number, originChainId: number) => {
+            if (this.fromForm.chainId === chainId) {
+                this.fromForm.changeChain(originChainId);
+            }
+            this.loadHistory();
+        });
+
+        this.loadHistory();
+        this.fromForm.on("connect", () => this.loadHistory());
+        this.toForm.on("connect", () => this.loadHistory());
     }
-  }
+
+    private loadHistoryNonce = 0;
+
+    private async loadHistory() {
+        if (
+            this.fromForm.sender !== undefined &&
+            this.toForm.sender !== undefined
+        ) {
+            const sender = await this.fromForm.sender.loadAddress();
+            const receiver = await this.toForm.sender.loadAddress();
+            if (sender !== undefined && receiver !== undefined) {
+                const count = await this.fromForm.sender.sendCount(
+                    sender,
+                    this.toForm.chainId,
+                    receiver
+                );
+                this.loadHistoryNonce += 1;
+                const nonce = this.loadHistoryNonce;
+                this.sendedList.empty();
+
+                SkyUtil.repeatResultAsync(count.toNumber(), async (sendId) => {
+                    if (this.loadHistoryNonce === nonce) {
+                        this.addSended(sender, receiver, BigNumber.from(sendId));
+                    }
+                });
+            }
+        }
+    }
+
+    public addSended(sender: string, receiver: string, sendId: BigNumber) {
+        if (
+            this.fromForm.sender !== undefined &&
+            this.toForm.sender !== undefined
+        ) {
+            new Sended(
+                this.fromForm.sender,
+                this.toForm.sender,
+                this.fromForm.chainId,
+                this.toForm.chainId,
+                sender,
+                receiver,
+                sendId.toNumber(),
+                async () => {
+                    if (this.fromForm.sender !== undefined) {
+                        const sended = await this.fromForm.sender.sended(
+                            sender,
+                            this.toForm.chainId,
+                            receiver,
+                            sendId
+                        );
+                        this.receiveOverHorizon(
+                            receiver,
+                            this.toForm.chainId,
+                            sender,
+                            sendId,
+                            sended
+                        );
+                    }
+                }
+            ).appendTo(this.sendedList, 0);
+        }
+    }
+
+    public async sendOverHorizon(amount: BigNumberish) {
+        if (
+            this.fromForm.sender !== undefined &&
+            this.toForm.sender !== undefined
+        ) {
+            const receiver = await this.toForm.sender.loadAddress();
+            if (receiver !== undefined) {
+                await this.fromForm.sender.sendOverHorizon(
+                    this.toForm.chainId,
+                    receiver,
+                    amount
+                );
+            }
+        }
+    }
+
+    public async receiveOverHorizon(
+        _receiver: string,
+        toChain: BigNumberish,
+        sender: string,
+        sendId: BigNumber,
+        amount: BigNumberish
+    ) {
+        if (
+            this.fromForm.sender !== undefined &&
+            this.toForm.sender !== undefined &&
+            this.toForm.chainId.toString() === toChain.toString()
+        ) {
+            const receiver = await this.toForm.sender.loadAddress();
+            if (receiver === _receiver) {
+                try {
+                    const result = await superagent
+                        .get(
+                            `https://api.chainhorizon.org/mix/signsend?receiver=${receiver}&fromChain=${this.fromForm.chainId
+                            }&toChain=${this.toForm.chainId
+                            }&sender=${sender}&sendId=${sendId}&amount=${amount.toString()}`
+                        )
+                        .send();
+                    await this.toForm.sender.receiveOverHorizon(
+                        this.fromForm.chainId,
+                        this.toForm.chainId,
+                        sender,
+                        sendId,
+                        amount,
+                        result.text
+                    );
+                } catch (error: any) {
+                    alert(`Error: ${error.message}`);
+                }
+            }
+        }
+    }
 }
