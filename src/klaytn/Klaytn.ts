6d71a1591e2a6239a546c7fed31911005e79ff49
Hanul <hanul@hanul.me>
2022-04-01 15:50:17 +0900

    Update Klaytn.ts

--- src/klaytn/Klaytn.ts
+++ src/klaytn/Klaytn.ts
@@ -1,18 +1,20 @@
 class Klaytn {
 
-    private caver = (window as any).caver !== undefined ? undefined :
-        new (window as any).Caver("https://api.dogesound.club:9013/");
+    private caver = new (window as any).Caver(new (window as any).Caver.providers.WebsocketProvider("wss://klaytn-node.klu.bs:9091", {
+        reconnect: {
+            auto: true,
+            delay: 1000,
+            maxAttempts: true,
+            onTimeout: false
+        },
+    }));
 
     public createContract(address: string, abi: any) {
-        return this.caver === undefined ?
-            new (window as any).caver.klay.Contract(abi, address) :
-            this.caver.contract.create(abi, address);
+        return this.caver.contract.create(abi, address);
     }
 
     public async loadBlockNumber() {
-        return this.caver === undefined ?
-            await (window as any).caver.klay.getBlockNumber() :
-            await this.caver.klay.getBlockNumber();
+        return await this.caver.klay.getBlockNumber();
     }
 }
 
