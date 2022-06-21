f875435d4d992ef48c4eb9b6b7390a4d128ef1b6
Sim Young Jae <hanul@hanul.me>
2022-06-21 15:51:22 +0900

    Update Klaytn.ts

--- src/klaytn/Klaytn.ts
+++ src/klaytn/Klaytn.ts
@@ -2,7 +2,7 @@ import { BigNumber } from "@ethersproject/bignumber";
 
 class Klaytn {
 
-    public caver = new (window as any).Caver(new (window as any).Caver.providers.WebsocketProvider("wss://klaytn01.fandom.finance/ws/", {
+    public caver = new (window as any).Caver(new (window as any).Caver.providers.WebsocketProvider("wss://klaytn04.fandom.finance/ws/", {
         reconnect: {
             auto: true,
             delay: 1000,
